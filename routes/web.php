a130efd3694db68b3d15cea9488206f09505fec1
Enzo Paiva <enzodouglaspaiva@gmail.com>
2020-10-27 00:46:47 -0300

    add migration and model

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->name('admin.')->middleware('can:manage-peoples')->group(function(){
+    Route::resource('/people', 'PeopleController');
+});
